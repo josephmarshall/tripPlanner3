f08a6ba5687ebd39bf1d64fe9c472d00137ff65d
Thanh Bui <ttthanhbui@gmail.com>
2019-03-09 11:12:37 -0700

    trips index controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,7 +1,37 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update, :destroy]
+
   def index
+    @trips = Trip.all
   end
 
   def show
   end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.create(trip_params)
+    if @trip.save
+      redirect_to @trip
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+    def set_trip
+      @trip = Trip.find(params[:id])
+    end
+
+    def trip_params
+      params(:trip).permit(:name, :start_date, :end_date)
+    end
 end
