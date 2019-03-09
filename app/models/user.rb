2c382098c0a09bf9221280dea7e592fd044040e7
Evan Wilson <wilsonevan100@gmail.com>
2019-03-09 15:12:19 -0700

    Added Logo to Nav and Names to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :trips, dependent: :destroy
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
 end
